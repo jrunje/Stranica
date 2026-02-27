93d60e47ee746f210f998783e5a09c8da8d2a0e6
Jelena Runje <jelena_bukovac@hotmail.com>
2026-02-27 15:26:21 +0100

    pocetak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Nav from "./components/Nav";
+import Naslovnica from "./pages/Naslovnica";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Naslovnica />} />
+        
+        
+
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ src/pages/Naslovnica.js
@@ -0,0 +1,41 @@
+import {useState, useEffect} from "react";
+import HeroSection from "../components/HeroSection";
+
+const BASE_URL = process.env.REACT_APP_API_URL
+
+const Naslovnica = () => {
+
+  const[page, setPage] = useState(null);
+
+  useEffect(() => {
+    const fetchPage = async() => {
+      try{
+        const response = await fetch(BASE_URL + 'v2/pages/?_embed');
+        if(!response.ok){
+          throw new Error('Ne mogu povući podatke');
+        }
+        const data = await response.json();
+        setPage(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    fetchPage();
+  }, []);
+
+  if(!page) return <p>Učitavanje...</p>;
+
+  return (
+    <>
+      <HeroSection 
+        stranica={page} 
+        fallback="https://placehold.co/600x400" 
+        size="full" 
+      />
+    
+      <div dangerouslySetInnerHTML={{ __html:page.content.rendered }} />
+    </>
+  );
+};
+
+export default Naslovnica;
