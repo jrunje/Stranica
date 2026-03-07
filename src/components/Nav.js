136e15d89553473471497966452b6a078aa7c970
Jelena Runje <jelena_bukovac@hotmail.com>
2026-03-07 14:53:19 +0100

    druga

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'; // Uvozimo komponente
-import logo from "../img/runje-logo.png";
+import logo from "../img/runje-logo.svg";
 import "./Nav.css";
 
 const Navigation = () => {
